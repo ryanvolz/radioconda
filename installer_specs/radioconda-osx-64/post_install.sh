64c9f9c0a08f16daf2dc481d4a8f61cbd887c8e3
Ryan Volz <ryan.volz@gmail.com>
2023-01-19 13:08:23 -0500

    Re-render 2023.01.19

--- installer_specs/radioconda-osx-64/post_install.sh
+++ installer_specs/radioconda-osx-64/post_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 PREFIX="${PREFIX:-$2/radioconda}"
-rm -f $PREFIX/pkgs/*.tar.bz2
+rm -f $PREFIX/pkgs/*.tar.bz2 $PREFIX/pkgs/*.conda
 exit 0
--- installer_specs/radioconda-win-64/post_install.bat
+++ installer_specs/radioconda-win-64/post_install.bat
@@ -1,3 +1,4 @@
 echo {"env_vars": {"GR_PREFIX": "", "GRC_BLOCKS_PATH": "", "UHD_PKG_PATH": "", "VOLK_PREFIX": ""}}>%PREFIX%\conda-meta\state
 del /q %PREFIX%\pkgs\*.tar.bz2
+del /q %PREFIX%\pkgs\*.conda
 exit 0
